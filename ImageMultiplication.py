a9fa53c9df6abe23ec5dac9b2468018429d7bb43
알파카 <rhehd721@naver.com>
2020-12-22 13:17:42 +0900

    이미지 증식코드(기존 좌표변환하여 txt로 저장해주고 이미지의 밝기와 상하좌우반전을 진행해줌)

--- ImageMultiplication.py
+++ ImageMultiplication.py
@@ -8,12 +8,12 @@ import os
 import cv2
 
 # 내가 읽을 폴더의 경로
-path_dir = '/home/user/Desktop/img/'
+path_dir = '/home/user/darknet/pbh/data/'
 
 # 폴더에 들어있는 파일리스트
 file_list = os.listdir(path_dir)
 
-# 파일과 파일에 해당하는 텍스트 분류
+# 사진과 사진에 해당하는 텍스트 분류
 img_list = [file for file in file_list if file.endswith(".jpg")]
 txt_list = [file for file in file_list if file.endswith(".txt")]
 
@@ -38,69 +38,84 @@ for i in range(len(img_list)):
     # 랜덤으로 조절할 밝기의 영역 설정
     datagen = ImageDataGenerator(brightness_range=[0.2,0.9])
 
-    # 숫자로 변환한 이미지에 밝기값을 조정한다
-    it = datagen.flow(samples, batch_size=1)
-
-    # 이건 아직 잘 모르겠다
-    batch = it.next()
-
-    # 숫자를 다시 이미지로 바꿔준다
-    image = batch[0].astype('uint8')
-
-    cv2.imwrite('/home/user/Desktop/save/'+img_list[i], image)
-
-######################## 여기서 다시 이미지를 읽어와야함 ############################ 
-    # 밝기 변화된 이미지 읽어오기
-    img = cv2.imread('/home/user/Desktop/save/'+img_list[i])
-
-    # 상하반전 0 
-    img = cv2.flip(img, 0)
-
-    # 좌우반전 1
-    img = cv2.flip(img, 1)
-
-    cv2.imwrite('/home/user/Desktop/save/'+img_list[i], img)
-######################## 반전과 밝기를 조정했다면 텍스트도 수정해줍니다. ############################ 
-
     # 텍스트파일 불러오기 (쓰기모드)
-    f = open(path_dir+ name[0] + '.txt', 'r')
-    line = f.readline()
-    print(line)
-    f.close()
-
-    # # 쓰기모드
-    # f = open("impo.txt", 'w')
-    # # for i in range(1, 1):
-    # f.write(line)
-    # f.close()
-
-
+    Origin = open(path_dir+ name[0] + '.txt', 'r')
+    OriginLine = Origin.readline()
+    OriginList = OriginLine.split(' ')
     
-# # 회전변수
-# rotation = 0
 
-# # 반전
-# X_reversal = 0
-# Y_reversal = 0
+    for j in range(4):
 
-# # 밝기
-# brightness = 0
+        # 숫자로 변환한 이미지에 밝기값을 조정한다
+        it = datagen.flow(samples, batch_size=1)
 
-# # 이동
-# X_move = 0
-# Y_move = 0
+        # 이건 아직 잘 모르겠다
+        batch = it.next()
 
-# # 텍스트파일 불러오기 (쓰기모드)
-# f = open("impo.txt", 'r')
-# line = f.readline()
-# print(line)
-# f.close()
+        # 숫자를 다시 이미지로 바꿔준다
+        image = batch[0].astype('uint8')
 
-# # 쓰기모드
-# f = open("impo.txt", 'w')
-# # for i in range(1, 1):
-# f.write(line)
-# f.close()
+        cv2.imwrite(path_dir+name[0]+'_00'+str(j)+'.jpg', image)
 
-# # 이미지를 90도 회전시킵니다
-# img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
+    ######################## 여기서 다시 이미지를 읽어와야함 ############################ 
+        # 밝기 변화된 이미지 읽어오기
+        img = cv2.imread(path_dir+name[0]+'_00'+str(j)+'.jpg')
+
+        if j == 0 :
+            # 쓰기모드
+            f = open(path_dir + name[0] +'_00'+str(j)+'.txt', 'w+')
+            f.write(OriginList[0] + ' ' + OriginList[1] +  ' ' + OriginList[2] +  ' ' + OriginList[3] +  ' ' + OriginList[4])
+            f.close()
+            pass
+
+        elif j == 1 :
+            # 상하반전 0 (X축 반전 즉, Y값 변경)
+            img = cv2.flip(img, 0)
+
+            # 쓰기모드
+            f = open(path_dir + name[0] +'_00'+str(j)+'.txt', 'w+')
+            if (float(OriginList[2]) > 0.5):
+                OriginList[2] = str(0.5 - (float(OriginList[2]) - 0.5))
+                pass
+            else:
+                OriginList[2] = str(0.5 + (0.5 - float(OriginList[2])))
+            f.write(OriginList[0] +  ' ' + OriginList[1] +  ' ' + OriginList[2] +  ' ' + OriginList[3] +  ' ' + OriginList[4])
+            f.close()
+
+        elif j == 2 :
+            # 상하반전 0 
+            img = cv2.flip(img, 0)
+            # 좌우반전 1
+            img = cv2.flip(img, 1)
+
+            # 쓰기모드
+            f = open(path_dir + name[0] +'_00'+str(j)+'.txt', 'w+')
+            if (float(OriginList[1]) > 0.5):
+                OriginList[1] = str(0.5 - (float(OriginList[1]) - 0.5))
+                pass
+            else:
+                OriginList[1] = str(0.5 + (0.5 - float(OriginList[1])))
+            if (float(OriginList[2]) > 0.5):
+                OriginList[2] = str(0.5 - (float(OriginList[2]) - 0.5))
+                pass
+            else:
+                OriginList[2] = str(0.5 + (0.5 - float(OriginList[2])))
+            f.write(OriginList[0] +  ' ' + OriginList[1] +  ' ' + OriginList[2] +  ' ' + OriginList[3] +  ' ' + OriginList[4])
+            f.close()
+
+        else:
+            # 좌우반전 1 (Y축 반전 즉, X값 변경)
+            img = cv2.flip(img, 1)
+
+            # 쓰기모드
+            f = open(path_dir + name[0] +'_00'+str(j)+'.txt', 'w+')
+            if (float(OriginList[1]) > 0.5):
+                OriginList[1] = str(0.5 - (float(OriginList[1]) - 0.5))
+                pass
+            else:
+                OriginList[1] = str(0.5 + (0.5 - float(OriginList[1])))
+            f.write(OriginList[0] +  ' ' + OriginList[1] +  ' ' + OriginList[2] +  ' ' + OriginList[3] +  ' ' + OriginList[4])
+            f.close()
+        
+
+        cv2.imwrite(path_dir+name[0]+'_00'+str(j)+'.jpg', img)
